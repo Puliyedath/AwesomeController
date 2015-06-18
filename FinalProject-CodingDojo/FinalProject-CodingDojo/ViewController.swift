7cd2547c14706d5a601c83aab65dac05da2fed0c
JV Estolas <fortis201@gmail.com>
2015-06-18 10:35:06 -0700

    first commit

---
+++ FinalProject-CodingDojo/FinalProject-CodingDojo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject-CodingDojo
+//
+//  Created by J.V. Estolas on 6/18/15.
+//  Copyright (c) 2015 J.V. Estolas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
